6184677321dd7dab31ecd0a87dc7dd376b706c9a
Colton Soneson <colton.soneson@gmail.com>
2024-04-04 17:54:42 -0400

    more primitive work

--- models/primitives.js
+++ models/primitives.js
@@ -1,5 +1,12 @@
-export const pSquare = {
-	vertices : 
+function Primitive(dimensions, vertices, indices) {
+	this.dimensions = dimensions;
+	this.vertices = vertices;
+	this.indices = indices;
+}
+
+//--------------------------2D----------------------------
+export const pSquare = new Primitive(
+	2, 
 	new Float32Array([
 		//   X,    Y,
 		-0.8, -0.8, // Triangle 1 (Blue)
@@ -10,13 +17,15 @@ export const pSquare = {
 		0.8,  0.8,
 		-0.8,  0.8,
 	]),
-};
+);
 
-export const pTriangle = {
-	vertices :
+export const pTriangle = new Primitive(
+	2,
 	new Float32Array([
 		0.0, 0.5,
 		-0.5, -0.5,
 		0.5, -0.5,
-	])
-}
+	]),
+);
+
+//--------------------------3D----------------------------

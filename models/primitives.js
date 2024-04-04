aa2264bef61413d7a28a0bd6394dce8d67e62074
Colton Soneson <colton.soneson@gmail.com>
2024-04-04 16:53:54 -0400

    primitives, seperate files for shaders, local server setup, seperate primitives list

---
+++ models/primitives.js
@@ -0,0 +1,22 @@
+export const pSquare = {
+	vertices : 
+	new Float32Array([
+		//   X,    Y,
+		-0.8, -0.8, // Triangle 1 (Blue)
+		0.8, -0.8,
+		0.8,  0.8,
+		
+		-0.8, -0.8, // Triangle 2 (Red)
+		0.8,  0.8,
+		-0.8,  0.8,
+	]),
+};
+
+export const pTriangle = {
+	vertices :
+	new Float32Array([
+		0.0, 0.5,
+		-0.5, -0.5,
+		0.5, -0.5,
+	])
+}
---
+++ webServerPythonScript.py
@@ -0,0 +1,16 @@
+#Use to create local host
+import http.server
+import socketserver
+
+#if files dont work, try another port (make sure its allowed through firewall)
+PORT = 1337
+
+#add any extensions needed here, just search ".extension MIME type" in google
+Handler = http.server.SimpleHTTPRequestHandler
+Handler.extensions_map.update({
+      ".js": "application/javascript",
+	  ".wgsl": "text/wgsl",
+});
+
+httpd = socketserver.TCPServer(("", PORT), Handler)
+httpd.serve_forever()

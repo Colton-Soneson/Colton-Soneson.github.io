b50621c835a557489efc0a124fb8d0453cead450
Colton Soneson <colton.soneson@gmail.com>
2024-04-06 16:10:43 -0400

    passing second binding in same group, cell state in u32 array

--- shaders/js/vf_p_gridGeneric.js
+++ shaders/js/vf_p_gridGeneric.js
@@ -1,6 +1,7 @@
 export const vf_p_gridGeneric = 
 `
 	@group(0) @binding(0) var<uniform> grid: vec2f;
+	@group(0) @binding(1) var<storage> cellState: array<u32>;	//same group as grid but different bind number
 
 	struct VertexInput {
 		@location(0) pos: vec2f,
@@ -17,8 +18,10 @@ export const vf_p_gridGeneric =
 												// "let" in WGSL behaves like const, "var" in wgsl is for changable variables
 		let i = f32(input.instance);								// convert to float
 		let cell = vec2f(i % grid.x, floor(i / grid.x));	// Cell(1,1) of grid
+		let state = f32(cellState[input.instance]);				//casting to f32
+		
 		let cellOffset = cell / grid * 2;					// offset to cell
-		let gridPos = (input.pos + 1) / grid - 1 + cellOffset;	
+		let gridPos = (input.pos * state + 1) / grid - 1 + cellOffset;	
 		
 		var output: VertexOutput;
 		output.pos = vec4f(gridPos, 0, 1);

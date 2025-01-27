15f93cbb7fc85169057c8af69c3d2a97e7f390b4
Colton Soneson <colton.soneson@gmail.com>
2025-01-26 23:57:38 -0500

    shadow pass set up with its own UBO, bind group layout, bind group, and shader

--- shaders/js/vf_p_generic.js
+++ shaders/js/vf_p_generic.js
@@ -93,3 +93,36 @@ export const vf_p_generic3D =
 		return textureSample(myTexture, mySampler, input.fragUV) * (Lights.sunCol * input.light);
 	}
 `;
+
+export const vf_p_shadowMap = 
+`
+
+	struct shadowUniforms
+	{
+		modelMatrix : mat4x4f,
+		lightViewProjMat : mat4x4f,
+	}
+	
+	@group(0) @binding(0) var<uniform> UBO: shadowUniforms;
+
+	struct VertexInput {
+		@location(0) pos: vec3f,
+		@location(1) uv: vec2f,
+		@location(2) norm: vec3f,
+	};
+	
+	struct VertexOutput {				
+		@builtin(position) pos: vec4f,
+	};
+	
+	@vertex
+	fn vertexMain(input: VertexInput) -> VertexOutput {	
+		var output: VertexOutput;
+		output.pos = UBO.lightViewProjMat * UBO.modelMatrix * vec4(input.pos, 1.0);
+    
+		return output;
+	}
+	
+	
+
+`;

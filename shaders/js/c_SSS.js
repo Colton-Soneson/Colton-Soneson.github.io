3325ba081599825597eef84b726a32c48e90e264
Colton Soneson <colton.soneson@gmail.com>
2025-02-04 20:59:25 -0500

    screen space compute shaders now work. Not pretty, but takes in swap chain image as storage read only texture then outputs to a write only texture. Once pass is done, then the resulting output texture is copied to the swap chain image. This is all done because the swapchain format can't support read-write

--- shaders/js/c_SSS.js
+++ shaders/js/c_SSS.js
@@ -1,16 +1,24 @@
 export const SSS_BUFFER_SIZE = 1000;
-export const SSS_WORKGROUP_SIZE = [64, 1, 1];
+export const SSS_WORKGROUP_SIZE = [16, 16, 1];
 
 export const c_SSS = 
 `
-@group(0) @binding(1) var texture : texture_storage_2d<rgba8unorm, read_write>;
+@group(0) @binding(1) var outTexture : texture_storage_2d<rgba8unorm, write>;
+@group(0) @binding(2) var inTexture : texture_storage_2d<rgba8unorm, read>;
+
+//@group(0) @binding(2) var inTexture : texture_2d<f32>;
+//@group(0) @binding(3) var mySampler : sampler;
 
 @compute @workgroup_size(${SSS_WORKGROUP_SIZE[0]}, ${SSS_WORKGROUP_SIZE[1]})	
     fn computeMain(
-		@builtin(global_invocation_id) GlobalIvocationID: vec3<u32>,
-		@builtin(workgroup_id) WorkGroupID : vec3u,
-		@builtin(local_invocation_id) LocalInvocationID : vec3u
+		@builtin(global_invocation_id) GlobalIvocationID: vec3<u32>
 	) {
+		let threadID = vec2<u32>(GlobalIvocationID.x, GlobalIvocationID.y);
 		
+		//read input texture
+		let inputCol = textureLoad(inTexture, vec2<u32>(threadID.x, threadID.y));
+		
+		//write to output
+		textureStore(outTexture, threadID, vec4<f32>(inputCol.r,inputCol.g,inputCol.b,1.0));
 	}
 `;

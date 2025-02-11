3326a07f1faf9fb5682f0f9bf3df2bef6f402b5f
Colton Soneson <colton.soneson@mymail.champlain.edu>
2025-02-10 19:10:59 -0500

    BROKEN COMMIT, but necesary. Compute shader needs to perform operations correctly on the passed in vertices. check if indexing is correct

--- shaders/js/vfc_grass.js
+++ shaders/js/vfc_grass.js
@@ -1,29 +1,80 @@
 import { settings } from '../../code/settings.js';
 
 export const GRASS_BUFFER_SIZE = 1000;
-export const GRASS_WORKGROUP_SIZE = [16, 16, 1];
+export const GRASS_WORKGROUP_SIZE = [16, 1, 1];
 
 export const c_grass = 
 `
-@compute @workgroup_size(${GRASS_WORKGROUP_SIZE[0]}, ${GRASS_WORKGROUP_SIZE[1]})	
+	struct GrassSpaces
+	{
+		modelViewProjectionMatrix : mat4x4f,
+	}
+	@group(0) @binding(0) var<uniform> Spaces: GrassSpaces;
+	
+	struct GrassUniforms {
+		@location(0) totalBladeCount: f32,
+		@location(1) density: f32,
+	};
+	@group(0) @binding(1) var<uniform> GU: GrassUniforms;
+	
+	
+	struct VertexData {
+		//has to match that of scene
+		pos: vec3f,
+		uv: vec2f,
+		norm: vec3f,
+	};
+	@group(0) @binding(2) var<storage, read> singleBladeVertexData: array<VertexData>;
+	
+	@group(0) @binding(3) var<storage, read_write> totalGrassVertexData: array<VertexData>;
+
+@compute @workgroup_size(${GRASS_WORKGROUP_SIZE[0]})	
     fn computeMain(
 		@builtin(global_invocation_id) GlobalIvocationID: vec3<u32>
 	) {
+		//1) create clump
+		//2) apply curves to each blade in each clump
+		//3) animate each clump
+		//4) disperse each clump
 		
+		
+		//for now, just take the single blade input, and output it to its index position in totalGrassVertexData
+		//		this is a 1-1 transaction, no loops to fill multiple points.
+		//		the DispatchWorkgroups call should be done enough times to then FILL this list of totalGrassVertexData, exactly
+		//			good test is 1024 blades, 16 workgroup size, leaving an exact 64 workgroup runs
+		
+		let myMat = mat4x4f(
+							1, 0, 0, 0,
+							0, 1, 0, 0, 
+							0, 0, 1, 0,
+							0, 0, 0, 1,
+							);
+		
+		//let outputPos = myMat * vec4f(singleBladeVertexData[GlobalIvocationID.x].pos.xyz + vec3f(-1,-1,0.0), 1.0);
+		//let outputPos = vec4f(singleBladeVertexData[GlobalIvocationID.x].pos.xyz + vec3f(-1,-1,0.0), 1.0);
+		//let outputPos = singleBladeVertexData[GlobalIvocationID.x].pos;
+		
+		let outputPos = Spaces.modelViewProjectionMatrix * vec4f(singleBladeVertexData[GlobalIvocationID.x].pos.xyz, 1.0);
+
+		
+		let temp = VertexData(outputPos.xyz,
+								singleBladeVertexData[GlobalIvocationID.x].uv,
+								singleBladeVertexData[GlobalIvocationID.x].norm);
+		
+		totalGrassVertexData[GlobalIvocationID.x] = temp;
 	}
 `;
 
 export const v_grass =
 `
-	struct GrassUniforms
+	//TODO: remove this after the input from compute shader comes in!!!!!!!!!!!!!
+	//			at that point, we wont have to do modelViewProjections at all, its just read already adjusted vertex data and output
+	struct GrassUniforms 
 	{
-		modelViewProjectionMatrix : mat4x4f, 	//IT IS UNECESSARY TO HAVE THIS BE PER BLADE
-												//	this should just be a default MVP with no pos, rot, or scale differences
-		
-		guPositions : array<vec4f, ${settings.grassTotalBladeCount}>
+		modelViewProjectionMatrix : mat4x4f,
 	}
 	@group(0) @binding(0) var<uniform> UBO: GrassUniforms;
-	
+		
 	struct VertexInput {
 		@builtin(instance_index) instanceIdx : u32,
 		@location(0) pos: vec3f,
@@ -42,14 +93,15 @@ export const v_grass =
 	fn vertexMain(input: VertexInput) -> VertexOutput {	
 	
 	var output: VertexOutput;
-    //output.pos = UBO.modelViewProjectionMatrix[input.instanceIdx] * vec4f(input.pos.x, input.pos.y, input.pos.z, 1.0);
+    //output.pos = UBO.modelViewProjectionMatrix * vec4f(input.pos.x, input.pos.y, input.pos.z, 1.0);
     
 	//INSTANCE TEST
+	//output.pos = UBO.modelViewProjectionMatrix * vec4f(input.pos.x + UBO.guPositions[input.instanceIdx].x, 
+	//																		input.pos.y + UBO.guPositions[input.instanceIdx].y, 
+	//																		input.pos.z + UBO.guPositions[input.instanceIdx].z, 
+	//																		1.0);
 	
-	output.pos = UBO.modelViewProjectionMatrix * vec4f(input.pos.x + UBO.guPositions[input.instanceIdx].x, 
-																			input.pos.y + UBO.guPositions[input.instanceIdx].y, 
-																			input.pos.z + UBO.guPositions[input.instanceIdx].z, 
-																			1.0);
+	output.pos = vec4f(input.pos.xyz, 1.0);
 	
     output.fragPos = output.pos;
 	output.fragNormal = input.norm;

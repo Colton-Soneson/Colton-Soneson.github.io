0aa28f01bda9d01a64f5bb97d552876948f52909
Colton Soneson <colton.soneson@gmail.com>
2025-02-11 19:12:34 -0500

    basic understanding of outputting to a vertex buffer from compute shader

--- shaders/js/vfc_grass.js
+++ shaders/js/vfc_grass.js
@@ -1,7 +1,7 @@
 import { settings } from '../../code/settings.js';
 
 export const GRASS_BUFFER_SIZE = 1000;
-export const GRASS_WORKGROUP_SIZE = [16, 1, 1];
+export const GRASS_WORKGROUP_SIZE = [1, 1, 1];
 
 export const c_grass = 
 `
@@ -26,42 +26,88 @@ export const c_grass =
 	};
 	@group(0) @binding(2) var<storage, read> singleBladeVertexData: array<VertexData>;
 	
-	@group(0) @binding(3) var<storage, read_write> totalGrassVertexData: array<VertexData>;
+	@group(0) @binding(3) var<storage, read_write> totalGrassVertexData: array<f32>;
 
 @compute @workgroup_size(${GRASS_WORKGROUP_SIZE[0]})	
     fn computeMain(
 		@builtin(global_invocation_id) GlobalIvocationID: vec3<u32>
 	) {
-		//1) create clump
-		//2) apply curves to each blade in each clump
-		//3) animate each clump
-		//4) disperse each clump
+		
+		var bladeIndex = GlobalIvocationID.x; // Single index for current invocation
+		let replicationCount = u32(GU.totalBladeCount);
+		
+		//let verticesPerBlade = u32(arrayLength(&singleBladeVertexData));
+		////let vertexIndex = bladeIndex % verticesPerBlade;
+		//
+		//let replicationFinalVertexIndex = bladeIndex * verticesPerBlade;
+		//
+		//if(bladeIndex >= replicationCount) {
+		//	return;
+		//}
+		//
+		////loop to go through every vertexData in singleBladeVertexData
+		//for(var singleBladeVertexDataIndex = 0u; singleBladeVertexDataIndex < verticesPerBlade; singleBladeVertexDataIndex++) {
+		//	
+		//	//translation to be made 
+		//	let translation = vec3f(5.0,0.0,0.0);
+		//	let outputPos = singleBladeVertexData[singleBladeVertexDataIndex].pos + translation;
+		//
+		//	//take a single vertexData from the singleBladeVertexData array, and adjust its TRS
+		//	let temp = VertexData(outputPos.xyz,
+		//							singleBladeVertexData[singleBladeVertexDataIndex].uv,
+		//							singleBladeVertexData[singleBladeVertexDataIndex].norm);
+		//	
+		//	//our current blade of grass, copying all the data of singleBladeVertexData when this loop completes
+		//	//	make sure to leave adjustment in position for the next blade of grass (replicationFinalVertexIndex)
+		//	totalGrassVertexData[replicationFinalVertexIndex + singleBladeVertexDataIndex] = temp;
+		//}
 		
 		
-		//for now, just take the single blade input, and output it to its index position in totalGrassVertexData
-		//		this is a 1-1 transaction, no loops to fill multiple points.
-		//		the DispatchWorkgroups call should be done enough times to then FILL this list of totalGrassVertexData, exactly
-		//			good test is 1024 blades, 16 workgroup size, leaving an exact 64 workgroup runs
+		//MAKE A FUCKING TRIANGLE AND REPLICATE TO SEE WHAT THE FUCK IS UP
+		var translatex = f32(bladeIndex) + 2.0;
+		let triangleTotalSize = 24u;
 		
-		let myMat = mat4x4f(
-							1, 0, 0, 0,
-							0, 1, 0, 0, 
-							0, 0, 1, 0,
-							0, 0, 0, 1,
-							);
+		//first pos
+		totalGrassVertexData[bladeIndex * triangleTotalSize + 0] = 0 + translatex;
+		totalGrassVertexData[bladeIndex * triangleTotalSize + 1] = 0.5;
+		totalGrassVertexData[bladeIndex * triangleTotalSize + 2] = 0;
 		
-		//let outputPos = myMat * vec4f(singleBladeVertexData[GlobalIvocationID.x].pos.xyz + vec3f(-1,-1,0.0), 1.0);
-		//let outputPos = vec4f(singleBladeVertexData[GlobalIvocationID.x].pos.xyz + vec3f(-1,-1,0.0), 1.0);
-		//let outputPos = singleBladeVertexData[GlobalIvocationID.x].pos;
+		//second pos
+		totalGrassVertexData[bladeIndex * triangleTotalSize + 8] = -0.5 + translatex;
+		totalGrassVertexData[bladeIndex * triangleTotalSize + 9] = -0.5;
+		totalGrassVertexData[bladeIndex * triangleTotalSize + 10] = 0;
 		
-		let outputPos = Spaces.modelViewProjectionMatrix * vec4f(singleBladeVertexData[GlobalIvocationID.x].pos.xyz, 1.0);
-
+		//third pos
+		totalGrassVertexData[bladeIndex * triangleTotalSize + 16] = 0.5 + translatex;
+		totalGrassVertexData[bladeIndex * triangleTotalSize + 17] = -0.5;
+		totalGrassVertexData[bladeIndex * triangleTotalSize + 18] = 0;
 		
-		let temp = VertexData(outputPos.xyz,
-								singleBladeVertexData[GlobalIvocationID.x].uv,
-								singleBladeVertexData[GlobalIvocationID.x].norm);
+		//first uv
+		totalGrassVertexData[bladeIndex * triangleTotalSize + 3] = 0.5;
+		totalGrassVertexData[bladeIndex * triangleTotalSize + 4] = 1;
 		
-		totalGrassVertexData[GlobalIvocationID.x] = temp;
+		//second uv
+		totalGrassVertexData[bladeIndex * triangleTotalSize + 11] = 0;
+		totalGrassVertexData[bladeIndex * triangleTotalSize + 12] = 0;
+		
+		//third uv
+		totalGrassVertexData[bladeIndex * triangleTotalSize + 19] = 1;
+		totalGrassVertexData[bladeIndex * triangleTotalSize + 20] = 0;
+		
+		//first norm
+		totalGrassVertexData[bladeIndex * triangleTotalSize + 5] = 0;
+		totalGrassVertexData[bladeIndex * triangleTotalSize + 6] = 0;
+		totalGrassVertexData[bladeIndex * triangleTotalSize + 7] = 1;
+		
+		//second norm
+		totalGrassVertexData[bladeIndex * triangleTotalSize + 13] = 0;
+		totalGrassVertexData[bladeIndex * triangleTotalSize + 14] = 0;
+		totalGrassVertexData[bladeIndex * triangleTotalSize + 15] = 1;
+		
+		//third norm
+		totalGrassVertexData[bladeIndex * triangleTotalSize + 21] = 0;
+		totalGrassVertexData[bladeIndex * triangleTotalSize + 22] = 0;
+		totalGrassVertexData[bladeIndex * triangleTotalSize + 23] = 1;
 	}
 `;
 
@@ -93,7 +139,7 @@ export const v_grass =
 	fn vertexMain(input: VertexInput) -> VertexOutput {	
 	
 	var output: VertexOutput;
-    //output.pos = UBO.modelViewProjectionMatrix * vec4f(input.pos.x, input.pos.y, input.pos.z, 1.0);
+    output.pos = UBO.modelViewProjectionMatrix * vec4f(input.pos.x, input.pos.y, input.pos.z, 1.0);
     
 	//INSTANCE TEST
 	//output.pos = UBO.modelViewProjectionMatrix * vec4f(input.pos.x + UBO.guPositions[input.instanceIdx].x, 
@@ -101,7 +147,7 @@ export const v_grass =
 	//																		input.pos.z + UBO.guPositions[input.instanceIdx].z, 
 	//																		1.0);
 	
-	output.pos = vec4f(input.pos.xyz, 1.0);
+	//output.pos = vec4f(input.pos.xyz, 1.0);
 	
     output.fragPos = output.pos;
 	output.fragNormal = input.norm;
